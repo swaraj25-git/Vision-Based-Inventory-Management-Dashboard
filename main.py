bcfff84e2a5a06bcf72e8d280544b3e2aa354bd9
swaraj25-git <swarajmahale25@gmail.com>
2026-06-17 17:51:45 +0530

    Phase2: Dividing the Canvas into 3 Sections

--- main.py
+++ main.py
@@ -7,14 +7,52 @@ import numpy as np
 FRAME_WIDTH = 1400
 FRAME_HEIGHT = 800
 
+TOP_BAR_HEIGHT = 60
+
+INCOMING_FEED_WIDTH = 350
+SHELF_LAYOUT_WIDTH = 650
+
+DASHBOARD_WIDTH = FRAME_WIDTH - INCOMING_FEED_WIDTH - SHELF_LAYOUT_WIDTH
+
+INCOMING_FEED_START_X = 0
+SHELF_LAYOUT_START_X = INCOMING_FEED_START_X + INCOMING_FEED_WIDTH
+DASHBOARD_START_X = SHELF_LAYOUT_START_X + SHELF_LAYOUT_WIDTH
+
+# UI  COLORS
+
+BG_DARK = (20,20,20)
+BORDER_GRAY = (80,80,80)
+TEXT_WHITE = (200,200,200)
+
+
 # camera feed
 CAM_X = 20
 CAM_Y = 80
 CAM_WIDTH = 350
 CAM_HEIGHT = 150
 
+
+
+# ui drawing functions
+def draw_top_bar(frame):
+    "Draws the top navigation bar"
+
+    cv2.rectangle(frame,(0,0),(FRAME_WIDTH,TOP_BAR_HEIGHT),BG_DARK,-1)
+
+    cv2.line(frame, (0,TOP_BAR_HEIGHT),(FRAME_WIDTH,TOP_BAR_HEIGHT),BORDER_GRAY,1)
+    cv2.putText(frame,"Vision Based Inventory Management System",(INCOMING_FEED_START_X+20, TOP_BAR_HEIGHT//2 +8),cv2.FONT_HERSHEY_SIMPLEX,0.8,TEXT_WHITE,2)
+
+def draw_main_sections_background(frame):
+    "Draws vertical lines to seperate 2 sections"
+    cv2.line(frame,(SHELF_LAYOUT_START_X,TOP_BAR_HEIGHT),(SHELF_LAYOUT_START_X,FRAME_HEIGHT),BORDER_GRAY,1)
+
+    cv2.line(frame,(DASHBOARD_START_X,TOP_BAR_HEIGHT),(DASHBOARD_START_X, FRAME_HEIGHT),BORDER_GRAY,1)
+
+
+
+
 def main():
-    cap = cv2.VideoCapture()
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not open webcam")
         return
@@ -30,14 +68,31 @@ def main():
 
         main_canvas = np.zeros((FRAME_HEIGHT, FRAME_WIDTH,3),dtype=np.uint8)
 
-        main_canvas[:] = (20,20,20)
 
-        resized_cam = cv2.resize(webcam_frame,(CAM_WIDTH,CAM_HEIGHT))
+        main_canvas[:] = BG_DARK
 
-        cam_x_end = CAM_X + CAM_WIDTH
-        cam_y_end = CAM_Y + CAM_HEIGHT
+        # draw the ui blueprint
+        draw_top_bar(main_canvas)
+        draw_main_sections_background(main_canvas)
 
-        main_canvas[CAM_Y:cam_y_end,CAM_X:cam_x_end] = resized_cam
+        # add title and paste the webcam
+        cv2.putText(main_canvas, "Incoming Feed",(INCOMING_FEED_START_X+20,TOP_BAR_HEIGHT + 30),cv2.FONT_HERSHEY_SIMPLEX,0.7,TEXT_WHITE,2)
+
+        # calculate the exact camera dimensions to fit perfectly inside the colum with padding
+        cam_x1 = INCOMING_FEED_START_X + 20
+        cam_y1 = TOP_BAR_HEIGHT + 60
+        cam_width = INCOMING_FEED_WIDTH - 40
+        cam_height = 150
+
+        resized_cam = cv2.resize(webcam_frame,(cam_width,cam_height))
+        main_canvas[cam_y1:cam_y1+cam_height,cam_x1:cam_x1+cam_width] = resized_cam
+
+        # show the canvas
+        #
+        # cam_x_end = CAM_X + CAM_WIDTH
+        # cam_y_end = CAM_Y + CAM_HEIGHT
+        #
+        # main_canvas[CAM_Y:cam_y_end,CAM_X:cam_x_end] = resized_cam
 
         cv2.imshow("Vision Based inventory management System",main_canvas)
 
